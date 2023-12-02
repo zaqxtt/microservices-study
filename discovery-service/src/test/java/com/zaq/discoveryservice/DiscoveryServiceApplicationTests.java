8862305382d4728217ac527e66afbeacfe189e75
zaq <117***328@qq.com>
2023-12-02 17:34:08 +0800

    Add eureka as service registration and discovery center

---
+++ discovery-service/src/test/java/com/zaq/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zaq.discoveryservice;
+
+import org.testng.annotations.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
